60532cf6b6ac3e2eb0fcac3e65790e6ce38df07d
Oriana <oriana@camba.coop>
2023-11-07 18:12:32 -0300

    day 6.1

--- day6/6.py
+++ day6/6.py
@@ -1,7 +1,5 @@
 with open("day6/6input.txt", "r") as file:
-    file_contents = file.readlines()[:7]
-
-
+    file_contents = file.read()
 
 class Device():
 
@@ -15,20 +13,20 @@ class Device():
 
         if len(self._data_stream) >= 4:
 
-            for i in range(4, len(self._data_stream)):
+            for i in range(4, len(self._data_stream) + 1):
 
                 comparable_characters = self._data_stream[(i-4): i]
-                
 
-
-
+                if (len(set(comparable_characters)) == 4):
+                    return (self._data_stream)
+            
+    
     def lock_onto_signal(self, signal):
         
         for character in signal:
             if self.start_of_packet_marker(character):
-                return "chi"
-
-
+                print(len(self.start_of_packet_marker(character))-1)
+                break
 
 
 
