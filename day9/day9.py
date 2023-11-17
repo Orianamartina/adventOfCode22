d51f99d410a1800d2018ff1d04b533e5ef7cece0
Oriana <oriana@camba.coop>
2023-11-17 09:28:27 -0300

    dia 9 terminado

--- day9/day9.py
+++ day9/day9.py
@@ -1,45 +1,6 @@
 with open("day9/9input.txt", "r") as file:
     file_contents = file.readlines()
 
-from math import sqrt
-
-class Knot:
-    def __init__(self, x, y):
-        self._current_position = [x, y]
-
-    @property
-    def current_position(self):
-        return self._current_position
-
-    def move_to(self, direction):
-        self._current_position[0] += ((direction == "R") - (direction == "L"))
-        self._current_position[1] += ((direction == "U") - (direction == "D"))
-
-    def follow(self, head):
-        head_x, head_y = head._current_position
-        x, y = self._current_position
-        _x = abs(head_x - x)
-        _y = abs(head_y - y)
-        if _x > 1 or _y > 1:
-           
-            self._current_position[0]+= (head_x > x) - (head_x < x)
-            self._current_position[1]+= (head_y > y) - (head_y < y) 
-
-def simulate_motions(list_of_motions, head, tail):
-    visited_coordinates = []
-    for motion in list_of_motions:
-        direction, ammount = motion
-        for _ in range(int(ammount)):
-            head.move_to(direction)
-            tail.follow(head)
-            coordinate = tail.current_position
-            if coordinate not in visited_coordinates:
-                visited_coordinates.append(coordinate)
-                
-
-    return len(visited_coordinates)
-
-
 def move(knot, direction):
 
     knot[0] += ((direction == "R") - (direction == "L"))
@@ -56,9 +17,11 @@ def follow(head, knot):
         
         knot[0] += (head_x > x) - (head_x < x)
         knot[1] += (head_y > y) - (head_y < y)
+
     return knot
 
-def simulate_motions_2(tails):
+def simulate_motions(rope_length):
+    tails = [[0, 0] for _ in range (rope_length)]
     visited_coordinates = []
     for motion in file_contents:
         direction, ammount = motion.split()
@@ -71,19 +34,8 @@ def simulate_motions_2(tails):
                     
             coordinate = tails[-1]
             visited_coordinates.append(tuple(coordinate))
-    print(visited_coordinates)
     return len(set(visited_coordinates))
 
-ex =[
-    ["R", "4"], ["U", "4"], ["L", "3"], ["D", "1"],["R", "4"],["D", "1"], ["L", "5"], ["R", "2"]
-]
-ex2=[
-    ["R", "5"], ["U", "8"], ["L", "8"], ["D", "3"], ["R", "17"], ["D", "10"], ["L", "25"], ["U", "20"]
-]
+print(simulate_motions(2))
 
-
-tails = [[0, 0] for _ in range(10)]
-
-#print(simulate_motions(instructions(), head, tail))
-
-print(simulate_motions_2(tails))
+print(simulate_motions(10))
